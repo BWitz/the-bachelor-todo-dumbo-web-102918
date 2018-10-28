0ae7351199b80fbf8e03e07e9713c4fb462513b0
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-28 23:28:51 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,7 +41,7 @@ end
 def get_average_age_for_season(data, season)
   sum_age = 0
   i = 0
-  data.each do |show, person|
+  data.each do |season_int, person|
     if seasons ==  season
       contestants.each do |contestant|
         sum_age += contestant["age"].to_i
